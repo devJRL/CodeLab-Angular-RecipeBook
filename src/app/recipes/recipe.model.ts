26340b67f9887d991a3842b0f2ae48f5b7aa2ec6
devJRL <dev2sponge@gmail.com>
2020-05-20 00:14:30 +0900

    HTML & Bootstrap3 with Model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+// Basic Blueprint for Model
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imgPath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imgPath;
+  }
+}
